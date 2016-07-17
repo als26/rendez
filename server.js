0cc2193cc3ea99d456aa4733e9aa1e80068b02cb
Aditya-Surabi <adityasurabi@gmail.com>
2016-07-16 21:07:51 -0400

    initial setup of socket.io

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+//var port = process.env.PORT || 3000;
 
 
 //Root route aka homepage. ex localhost:3000
 app.get('/', function(req, res){
-  res.send("It's alive, run!");
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port);
+io.on('connection', function(socket){
+  console.log('A user connected');
+
+  socket.on('disconnect', function(){
+
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000,function(){
+console.log('Magic happens on port 3000')
+});
