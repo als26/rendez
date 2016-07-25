74a9c30a0f99185139c407d9af25be83d13c16cb
Abilas <abilas96@gmail.com>
2016-07-24 22:17:19 -0400

    storing user's name

--- public/app.js
+++ public/app.js
@@ -2,11 +2,21 @@ jQuery( document ).ready(function() {
   console.log("script ready test");
   var socket = io();
   var room = "abc123";
+  var userName = null;
   socket.on('connect', function () {
     socket.emit('room', room);
   });
 
   socket.on('message', function (data) {
     console.log("The message is" + data);
+  });
+  
+  $("#create-button").bind('click',function() {
+      $("#user-name").css('visibility', 'visible');
+  });
+  
+  $("#user-name-submit").bind('click', function() {
+    userName = $("#user-name-input").val();
+    console.log("user name: " +userName);
   });
 });
